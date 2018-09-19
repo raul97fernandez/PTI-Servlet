e114360a09b079dd6df71fd4de3b46af0f306cea
Raul Fernández <raul97fernandez@gmail.com>
2018-09-19 09:28:37 +0200

    Udpdated List CarRental

--- my_webapp/WEB-INF/classes/mypackage/CarRentalList.java
+++ my_webapp/WEB-INF/classes/mypackage/CarRentalList.java
@@ -4,6 +4,16 @@ import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import java.util.Iterator;
+
+import java.io.FileReader;
+import org.json.simple.parser.JSONParser;
+import java.io.FileNotFoundException;
+import org.json.simple.parser.ParseException;
+
 public class CarRentalList extends HttpServlet {
 
   int cont = 0;
@@ -14,8 +24,24 @@ public class CarRentalList extends HttpServlet {
     PrintWriter out = res.getWriter();
     String nombre = req.getParameter("userid");
     cont ++;
-    out.println("<html><big>Hola Amigo "+ nombre + "</big><br>"+
-                cont + " Accesos desde su carga.</html>");
+    out.println("<html><big> Bienvenido "+ nombre + "</big>");
+    
+    JSONParser parser = new JSONParser();
+    JSONObject obj = null;
+    
+    try {
+    Object obj2 = parser.parse(new FileReader("./test.json"));
+       obj = (JSONObject) obj2;
+       JSONArray list = (JSONArray) obj.get("messages");
+    Iterator<String> iterator = list.iterator();
+    while(iterator.hasNext()){
+			out.println("<br> <big>" + iterator.next() + "</big>");
+		}
+	out.println("</html>");
+}
+catch (ParseException e) {
+            e.printStackTrace();
+        }
   }
 
   public void doPost(HttpServletRequest req, HttpServletResponse res)
